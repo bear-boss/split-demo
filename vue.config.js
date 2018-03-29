f5da52cc1cc7c06cbbfc87f8192b8b9dcb58d09d
Chen Guixiong <757858509@qq.com>
2018-03-29 14:35:18 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,3 @@
-var webpack = require('webpack')
 module.exports = {
-  lintOnSave: true,
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'jquery': 'jquery'
-      }
-    },
-    plugins: [
-      new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        jquery: 'jquery',
-        'window.jQuery': 'jquery'
-      }) ]
-  }
+  lintOnSave: false
 }
